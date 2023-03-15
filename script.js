f4674a5c2108a45c72f2702f58695b8c70fd876d
Your Name Here <your_email@youremail.com>
2023-03-15 10:26:01 -0400

    Finished assignment

---
+++ script.js
@@ -0,0 +1,56 @@
+// 1
+console.log(13);
+
+// 2
+// Boolean, String, Number, Null, Undefined
+
+// 3
+// =, -, *, /, **, %
+
+// 4
+console.log(5 % 2); // 1
+console.log(5 % 4); // 1
+
+// 5
+console.log('I\'M MARY POPPINS Y\'ALL');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Hayden".length + "Leet".length);
+
+// 8
+"Avengers: Endgame"[16];
+
+// BONUS
+// 1
+console.log("\\");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+console.log((("I".length + "AM".length) % "IRON".length) % "MAN".length) // 0
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+// var m = 25;
+// var c = 4;
+// var u = m + c
+// console.log(u);
+
+// USING LET
+let m = 25;
+let c = 4;
+let u = m + c
+console.log(u);
